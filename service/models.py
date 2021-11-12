e4de2c6b19e0290f51296258bfcb9a8f9e5b0b24
Dltmd202 <dltmd202@gmail.com>
2021-11-12 11:18:01 +0900

    서비스 로직 모델 생성

--- service/models.py
+++ service/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    score = models.FloatField()
+    contents = models.TextField()
+
+
+class Waiting(models.Model):
+    restaurant = models.ForeignKey('Restaurant', on_delete=models.CASCADE)
+    leader = models.OneToOneField('Guest',
+                                  on_delete=models.CASCADE,
+                                  related_name='leader')
+    member = models.ManyToManyField('Guest',
+                                    related_name='member')
+
+
+class Acceptation(models.Model):
+    admission_date = models.DateTimeField(auto_now=True)
+    waiting = models.ForeignKey(Waiting, on_delete=models.CASCADE)
