229da957e2fccd320def6b8ceba95e485e07167e
sa11k <sdk4590@gmail.com>
2021-11-14 17:51:31 +0900

    account admin, serializers, urls 수정 및 API 명세서 링크 추가

--- account/urls.py
+++ account/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+from account import views
+
+urlpatterns = [
+    path('guest/', views.GuestList.as_view()),
+    path('guest/<str:username>', views.GuestDetail.as_view()),
+    path('restaurant/', views.RestaurantList.as_view()),
+    path('restaurant/<int:pk>', views.RestaurantDetail.as_view()),
+]
