10e9f6a576badf9a1e5c3737be98468257543bad
Dltmd202 <dltmd202@gmail.com>
2021-11-16 04:02:48 +0900

    수락 컨트롤러 생성

--- service/views.py
+++ service/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from rest_framework import generics
-from service.models import Waiting
-from service.serializers import WaitingSerializer
+from rest_framework import generics, permissions, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+
+from account.models import Restaurant
+from service.models import Waiting, Acceptation
+from service.serializers import WaitingSerializer, AcceptationSerializer
+
+from account.permissions import IsHostOrReadOnly
 
 
 # Create your views here.
@@ -14,3 +20,24 @@ class WaitingDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Waiting.objects.all()
     serializer_class = WaitingSerializer
 
+
+@api_view(['GET', 'POST'])
+@permission_classes([permissions.IsAuthenticated, IsHostOrReadOnly])
+def accept_waiting(request, restaurant_pk):
+    if request.method == 'GET':
+        acceptation = Acceptation.objects.filter(restaurant=restaurant_pk)
+        serializer = AcceptationSerializer(acceptation, many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        restaurant = Restaurant.objects.get(pk=restaurant_pk)
+        waiting = Waiting.objects.filter(accepted=False)\
+            .order_by('-date')[0]
+        acceptation = Acceptation.objects.create(
+            waiting=waiting
+        )
+        restaurant.acceptation.add(acceptation)
+        waiting.accepted = True
+        serializer = AcceptationSerializer(data=acceptation)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
