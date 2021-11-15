9798716891e92947b2a69d3e2611da8ccf1d8789
Dltmd202 <dltmd202@gmail.com>
2021-11-16 04:03:53 +0900

    수락 컨트롤러 url 매핑

--- service/urls.py
+++ service/urls.py
@@ -4,4 +4,5 @@ from service import views
 urlpatterns = [
     path('waiting/', views.WaitingList.as_view()),
     path('waiting/<int:pk>/', views.WaitingDetail.as_view()),
+    path('restaurant/waiting/<int:restaurant_pk>', views.accept_waiting),
 ]
