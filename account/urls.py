f3657e3e17d46eb992fe3e32bd9b13bdb052bee7
Dltmd202 <dltmd202@gmail.com>
2021-11-16 14:57:20 +0900

    계정 컨트롤러 url 매핑

--- account/urls.py
+++ account/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from account import views
 
 urlpatterns = [
+    path('login/', views.login),
     path('guest/', views.GuestList.as_view()),
-    path('guest/<str:username>', views.GuestDetail.as_view()),
+    path('guest/<int:pk>', views.GuestDetailPK.as_view()),
+    path('guest/<str:username>', views.GuestDetailUN.as_view()),
     path('restaurant/', views.RestaurantList.as_view()),
     path('restaurant/<int:pk>', views.RestaurantDetail.as_view()),
 ]
