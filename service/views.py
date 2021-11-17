3bd7be6113ea4284d6086fe518f0d6524a8da515
Dltmd202 <dltmd202@gmail.com>
2021-11-18 00:08:09 +0900

    수락 로직 날짜를 당일로 한정

--- service/views.py
+++ service/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+import datetime
 from rest_framework import generics, permissions, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from django.utils.timezone import utc
 
 from account.models import Restaurant
 from service.models import Waiting, Acceptation
@@ -24,13 +26,14 @@ class WaitingDetail(generics.RetrieveUpdateDestroyAPIView):
 @api_view(['GET', 'POST'])
 @permission_classes([permissions.IsAuthenticated, IsHostOrReadOnly])
 def accept_waiting(request, restaurant_pk):
+    now = datetime.datetime.utcnow().replace(tzinfo=utc).date()
     if request.method == 'GET':
-        acceptation = Acceptation.objects.filter(restaurant=restaurant_pk)
+        acceptation = Acceptation.objects.filter(restaurant=restaurant_pk).filter(admission_date__day=now.day)
         serializer = AcceptationSerializer(acceptation, many=True)
         return Response(serializer.data)
     if request.method == 'POST':
         restaurant = Restaurant.objects.get(pk=restaurant_pk)
-        waiting = Waiting.objects.filter(accepted=False)\
+        waiting = Waiting.objects.filter(accepted=False).filter(date__day=now.day)\
             .order_by('-date')[0]
         acceptation = Acceptation.objects.create(
             waiting=waiting
