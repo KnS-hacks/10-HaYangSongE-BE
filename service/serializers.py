5b658180ad0e2c1ece0becc561aebb748e17a3e7
Dltmd202 <dltmd202@gmail.com>
2021-11-19 20:47:06 +0900

    식당 사진 추가

--- service/serializers.py
+++ service/serializers.py
@@ -158,7 +158,7 @@ class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
         fields = ['id', 'name', 'host', 'phone_number', 'branch_name', 'district', 'area',
-                  'waiting_avg', 'total_seat', 'remain_seat', 'menu',
+                  'waiting_avg', 'total_seat', 'remain_seat', 'menu', 'restaurant_photo',
                   'waitings', 'acceptation', 'vaccine_condition', 'SE', 'WN']
 
     def create(self, validated_data):
